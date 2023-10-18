bb5615bb01e503bfb18ca29fc9b4e2800534cf02
YesidWork2804 <yesidwork2804@gmail.com>
2023-10-18 17:38:09 -0500

    cambios

--- src/app/file-upload/FileUploadComponent.ts
+++ src/app/file-upload/FileUploadComponent.ts
@@ -9,43 +9,51 @@ import { Component, OnInit } from '@angular/core';
 // implements OnInit
 export class FileUploadComponent implements OnInit {
   selectedFile: File | null = null;
-
-  ngOnInit() {
-    this.getFiles(); // Llama a getFiles() cuando el componente se inicia
-  }
-
   fileList: fileInfo[] = [];
   constructor(private http: HttpClient) {}
+
+  uploadSuccessMessage: string | null = null;
+
+  ngOnInit() {
+    this.getFiles();
+  }
 
   onFileSelected(event: any) {
     this.selectedFile = event.target.files[0];
   }
 
-  uploadFile() {
+  async uploadFile() {
+    // console.log(this.selectedFile);
     if (this.selectedFile) {
       const formData = new FormData();
       formData.append('file', this.selectedFile);
 
-      this.fileList.push(this.selectedFile);
       this.http.post('http://localhost:3000/upload', formData).subscribe(
         (response: any) => {
           console.log('Archivo subido con éxito:', response.filename);
+          this.getFiles();
         },
         (error) => {
           console.error('Error al subir el archivo:', error);
         }
       );
-      this.selectedFile = null; // Limpiar el archivo seleccionado después de agregarlo a la lista
+      this.uploadSuccessMessage = '¡Archivo subido con éxito!';
+      this.selectedFile = null;
+
+      // Limpiar el archivo seleccionado después de agregarlo a la lista
     } else {
+      this.uploadSuccessMessage =
+        'Error al subir el archivo. Por favor, inténtelo de nuevo.';
       console.log('No se ha seleccionado ningún archivo.');
     }
   }
 
   getFiles() {
-    this.http.get('http://localhost:3000/upload/').subscribe(
+    this.http.get('http://localhost:3000/files').subscribe(
       (response: any) => {
-        response.forEach((file: any) => {
-          this.fileList.push({ name: file });
+        this.fileList = [];
+        response.forEach((file: fileInfo) => {
+          this.fileList.push({ id: file.id, name: file.name, path: file.path });
         }),
           console.log('Archivos descargados con éxito:', response);
       },
@@ -55,10 +63,20 @@ export class FileUploadComponent implements OnInit {
     );
   }
 
-  removeFile(index: number) {
-    this.fileList.splice(index, 1);
+  removeFile(id: string) {
+    this.http
+      .delete('http://localhost:3000/files/' + id)
+      .subscribe((response: any) => {
+        console.log('Archivo eliminado con éxito:', response);
+        this.getFiles();
+      });
+  }
+  clearUploadSuccessMessage() {
+    this.uploadSuccessMessage = null;
   }
 }
 interface fileInfo {
+  id: string;
   name: string;
+  path: string;
 }
